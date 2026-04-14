23dc93eb1ef6ac84ab0a7fb7ca8f8dab4178c444
Akram H. Ahmed <sweayup@gmail.com>
2026-04-14 22:19:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ─────────────────────────────────────────────────────────────────────────────
-// IMPORTANT: Set `base` to match your GitHub repo name exactly.
-// e.g. if the repo URL is https://github.com/username/emre-gateway
-// then base should be '/emre-gateway/'
-// ─────────────────────────────────────────────────────────────────────────────
 export default defineConfig({
   plugins: [react()],
   base: '/GreenHub/',
